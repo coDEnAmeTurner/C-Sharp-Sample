4351fa8c76f99cc4dee2e7689231b0ea022ba1b4
coDEnAmeTurner <tranluuquoctuan191191@gmail.com>
2025-08-30 16:37:00 +0700

    done testing async await

--- C-Sharp-Sample/Program.cs
+++ C-Sharp-Sample/Program.cs
@@ -1,2 +1,96 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace CSharpSample
+{
+    internal class Program
+    {
+
+        internal class HashBrown { }
+        internal class Coffee { }
+        internal class Egg { }
+        internal class Juice { }
+        internal class Toast { }
+        private static async Task Main(string[] args)
+        {
+            Console.WriteLine($"Start cooking: {DateTime.Now.ToString("hh:mm:ss.ffff")}");
+            Coffee cup = PourCoffee();
+            Console.WriteLine("coffee is ready");
+
+            Task<Egg> eggTask = FryEggsAsync(2);
+            Task<HashBrown> hashBrownTask = FryHashBrownsAsync(3);
+            Task<Toast> toastTask = ToastBreadAsync(2);
+
+            Toast toast = await toastTask;
+            ApplyButter(toast);
+            ApplyJam(toast);
+            Console.WriteLine("toast is ready");
+            Juice oj = PourOJ();
+            Console.WriteLine("oj is ready");
+
+            HashBrown hashbrown = await hashBrownTask;
+            Console.WriteLine("hash browns are ready");
+            Egg eggs = await eggTask;
+            Console.WriteLine("eggs are ready");
+
+            Console.WriteLine("Breakfast is ready!");
+
+            Console.WriteLine($"Done cooking: {DateTime.Now.ToString("hh:mm:ss.ffff")}");
+        }
+        private static Juice PourOJ()
+        {
+            Console.WriteLine("Pouring orange juice");
+            return new Juice();
+        }
+
+        private static void ApplyJam(Toast toast) =>
+            Console.WriteLine("Putting jam on the toast");
+
+        private static void ApplyButter(Toast toast) =>
+            Console.WriteLine("Putting butter on the toast");
+
+        private static async Task<Toast> ToastBreadAsync(int slices)
+        {
+            for (int slice = 0; slice < slices; slice++)
+            {
+                Console.WriteLine("Putting a slice of bread in the toaster");
+            }
+            Console.WriteLine("Start toasting...");
+            Task.Delay(3000).Wait();
+            Console.WriteLine("Remove toast from toaster");
+
+            return new Toast();
+        }
+
+        private static async Task<HashBrown> FryHashBrownsAsync(int patties)
+        {
+            Console.WriteLine($"putting {patties} hash brown patties in the pan");
+            Console.WriteLine("cooking first side of hash browns...");
+            Task.Delay(5000).Wait();
+            for (int patty = 0; patty < patties; patty++)
+            {
+                Console.WriteLine("flipping a hash brown patty");
+            }
+            Console.WriteLine("cooking the second side of hash browns...");
+            Task.Delay(5000).Wait();
+            Console.WriteLine("Put hash browns on plate");
+
+            return new HashBrown();
+        }
+
+        private static async Task<Egg> FryEggsAsync(int howMany)
+        {
+            Console.WriteLine("Warming the egg pan...");
+            Task.Delay(8000).Wait();
+            Console.WriteLine($"cracking {howMany} eggs");
+            Console.WriteLine("cooking the eggs ...");
+            Task.Delay(8000).Wait();
+            Console.WriteLine("Put eggs on plate");
+
+            return new Egg();
+        }
+
+        private static Coffee PourCoffee()
+        {
+            Console.WriteLine("Pouring coffee");
+            return new Coffee();
+        }
+    }
+}
